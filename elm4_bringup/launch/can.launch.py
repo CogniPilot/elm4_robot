e0d7a7ef26592f1802f7c490bdccf61a15917147
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2023-05-16 19:32:43 -0400

    Add robot bringup launch files.
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

---
+++ elm4_bringup/launch/can.launch.py
@@ -0,0 +1,86 @@
+import os
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchContext, LaunchDescription, SomeSubstitutionsType, Substitution
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.conditions import IfCondition, LaunchConfigurationEquals
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+
+ARGUMENTS = [
+    DeclareLaunchArgument('leds', default_value='true',
+                          choices=['true', 'false'],
+                          description='Run LEDs.'),
+    DeclareLaunchArgument('vesc', default_value='true',
+                          choices=['true', 'false'],
+                          description='Run VESC can control')
+]
+
+
+def generate_launch_description():
+
+    opencyphal_led_node = Node(
+        package='opencyphal_led',
+        executable='opencyphal_led_node',
+        name='opencyphal_led_node_0',
+        output='screen',
+        parameters=[
+            {"led_brightness": 5},
+            {"led_image_topic": "/led_image"},
+            {"joy_topic": "/joy"},
+            {"cyphal_topic": "/CyphalTransmitFrame"},
+            {"max_leds": 12}],
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration("leds"))
+    )
+
+    opencyphal_send_node = Node(
+        package='opencyphal_send',
+        executable='opencyphal_send_node',
+        name='opencyphal_send_0',
+        output='screen',
+        parameters=[
+            {"cyphal_input_topic": "/CyphalTransmitFrame"},
+            {"can_channel": "can1"}],
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration("leds"))
+    )
+
+    vesc_can_control_node = Node(
+        package='vesc_can_control',
+        executable='vesc_can_control_node',
+        name='vesc_can_control_node0',
+        output='screen',
+        parameters=[
+            {"actuators_input_topic": "/actuators"},
+            {"can_output_topic": "/can0_send"},
+            {"number_vesc": 4},
+            {"actuators_index_can": [0, 1, 2, 3]},
+            {"actuators_pole_pair": [15, 15, 15, 15]},
+            {"can_node_id_actuators": [119, 85, 73, 23]}],
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration("vesc"))
+    )
+
+    can_send_node = Node(
+        package='can_send',
+        executable='can_send_node',
+        name='can_send_node0',
+        output='screen',
+        parameters=[
+            {"can_channel": "can0"},
+            {"can_input_topic": "/can0_send"}],
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration("vesc"))
+    )
+
+    # Define LaunchDescription variable
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(opencyphal_led_node)
+    ld.add_action(opencyphal_send_node)
+    ld.add_action(vesc_can_control_node)
+    ld.add_action(can_send_node)
+    return ld
+
---
+++ elm4_bringup/launch/laser.launch.py
@@ -0,0 +1,91 @@
+import os
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchContext, LaunchDescription, SomeSubstitutionsType, Substitution
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.conditions import IfCondition, LaunchConfigurationEquals
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+
+ARGUMENTS = [
+    DeclareLaunchArgument('stl27l', default_value='true',
+                          choices=['true', 'false'],
+                          description='Run STL27L.'),
+    DeclareLaunchArgument('rf2o', default_value='true',
+                          choices=['true', 'false'],
+                          description='Run rf2o laser odometry'),
+    DeclareLaunchArgument('stl27l_tf', default_value='true',
+                          choices=['true', 'false'],
+                          description='Run STL27L TF.'),
+    DeclareLaunchArgument('rf2o_tf', default_value='true',
+                          choices=['true', 'false'],
+                          description='Run rf2o laser odometry TF'),
+    DeclareLaunchArgument('use_sim_time', default_value='false',
+                          choices=['true', 'false'],
+                          description='use_sim_time')
+]
+
+
+def generate_launch_description():
+
+    stl27l_node = Node(
+        package='ldlidar_stl_ros2',
+        executable='ldlidar_stl_ros2_node',
+        name='STL27L',
+        output='screen',
+        parameters=[
+            {'product_name': 'LDLiDAR_STL27L'},
+            {'topic_name': 'scan'},
+            {'frame_id': 'base_laser'},
+            {'port_name': '/dev/ttymcx0'},
+            {'port_baudrate': 921600},
+            {'laser_scan_dir': False},
+            {'enable_angle_crop_func': False},
+            {'angle_crop_min': 0.0},
+            {'angle_crop_max': 0.0}],
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration("stl27l"))
+    )
+
+    rf2o_odom_node = Node(
+        package='rf2o_laser_odometry',
+        executable='rf2o_laser_odometry_node',
+        name='rf2o_laser_odometry',
+        output='screen',
+        parameters=[{
+            'laser_scan_topic' : '/scan',
+            'odom_topic' : '/odom_rf2o',
+            'publish_tf' : LaunchConfiguration('rf2o_tf'),
+            'base_frame_id' : 'base_link',
+            'odom_frame_id' : 'odom',
+            'init_pose_from_topic' : '',
+            'freq' : 20.0,
+            'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration("rf2o"))
+    )
+
+    stl27l_tf_node = Node(
+        name='base_link_to_base_laser_stl27l',
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=[
+            '--x', '0.0', '--y', '0.0', '--z', '0.0',
+            '--roll', '0.0', '--pitch', '0.0', '--yaw', '0.0',
+            '--frame-id', 'elm4/base_laser', '--child-frame-id', 'baselink'],
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration("stl27l_tf"))
+    )
+
+
+    # Define LaunchDescription variable
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(stl27l_node)
+    ld.add_action(rf2o_odom)
+    ld.add_action(stl27l_tf_node)
+    return ld
+
